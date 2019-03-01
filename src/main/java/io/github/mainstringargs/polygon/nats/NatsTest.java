032224bd4d19ef5b7f6cfbcd7b4e897af0857053
mainstringargs <mark.l.cloutier@gmail.com>
2019-03-01 14:40:47 -0600

    Allow setting of ticker by arg

--- src/main/java/io/github/mainstringargs/polygon/nats/NatsTest.java
+++ src/main/java/io/github/mainstringargs/polygon/nats/NatsTest.java
@@ -12,6 +12,12 @@ public class NatsTest {
 
   public static void main(String[] args) {
 
+    String ticker = "QQQ";
+
+    if (args.length == 1) {
+      ticker = args[0];
+    }
+
     Options o = new Options.Builder()
         .server("nats://" + AlpacaProperties.KEY_ID_VALUE + "@nats1.polygon.io:31101")
         .server("nats://" + AlpacaProperties.KEY_ID_VALUE + "@nats2.polygon.io:31102")
@@ -28,15 +34,13 @@ public class NatsTest {
       });
 
 
-      d.subscribe("Q.QQQ"); // quotes
+      d.subscribe("Q." + ticker); // quotes
       // d.subscribe("T.QQQ");
       // d.subscribe("A.QQQ");
       // d.subscribe("AM.QQQ");
 
 
-    } catch (IOException e) {
-      e.printStackTrace();
-    } catch (InterruptedException e) {
+    } catch (Exception e) {
       e.printStackTrace();
     }
   }
