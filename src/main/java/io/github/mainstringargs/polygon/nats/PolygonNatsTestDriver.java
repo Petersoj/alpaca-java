00be66b9e0e740bbb094990cd9f1c02ccc6fa718
robfrank <ro.franchini@gmail.com>
2019-04-12 14:47:28 +0200

    fixex compilation error with Sets.newHashSet( from guava

--- src/main/java/io/github/mainstringargs/polygon/nats/PolygonNatsTestDriver.java
+++ src/main/java/io/github/mainstringargs/polygon/nats/PolygonNatsTestDriver.java
@@ -1,123 +1,125 @@
 package io.github.mainstringargs.polygon.nats;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import com.google.common.collect.Sets;
 import io.github.mainstringargs.polygon.enums.ChannelType;
 import io.github.mainstringargs.polygon.nats.message.ChannelMessage;
 import io.github.mainstringargs.polygon.properties.PolygonProperties;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 
 public class PolygonNatsTestDriver {
 
-  /**
-   * The main method.
-   *
-   * @param args the arguments
-   */
-  public static void main(String[] args) {
+    /**
+     * The main method.
+     *
+     * @param args the arguments
+     */
+    public static void main(String[] args) {
 
-    PolygonNatsClient client = new PolygonNatsClient(PolygonProperties.KEY_ID_VALUE,
-        PolygonProperties.POLYGON_NATS_SERVERS_VALUE);
+        PolygonNatsClient client = new PolygonNatsClient(PolygonProperties.KEY_ID_VALUE,
+                PolygonProperties.POLYGON_NATS_SERVERS_VALUE);
 
 
-    PolygonStreamListener listener1 = new PolygonStreamListener() {
+        PolygonStreamListener listener1 = new PolygonStreamListener() {
 
-      @Override
-      public void streamUpdate(String ticker, ChannelType channelType, ChannelMessage message) {
-        System.out.println(ticker + " " + channelType + " " + message);
-      }
+            @Override
+            public void streamUpdate(String ticker, ChannelType channelType, ChannelMessage message) {
+                System.out.println(ticker + " " + channelType + " " + message);
+            }
 
-      @Override
-      public Map<String, Set<ChannelType>> getStockChannelTypes() {
+            @Override
+            public Map<String, Set<ChannelType>> getStockChannelTypes() {
 
-        Map<String, Set<ChannelType>> subscribedTypes = new HashMap<>();
+                Map<String, Set<ChannelType>> subscribedTypes = new HashMap<>();
 
-        subscribedTypes.put("SNAP", Set.of(ChannelType.values()));
-        subscribedTypes.put("AMZN",
-            Set.of(ChannelType.AGGREGATE_PER_MINUTE, ChannelType.AGGREGATE_PER_SECOND));
+                subscribedTypes.put("SNAP", Sets.newHashSet(ChannelType.values()));
+                subscribedTypes.put("AMZN",
+                        Sets.newHashSet(ChannelType.AGGREGATE_PER_MINUTE, ChannelType.AGGREGATE_PER_SECOND));
 
-        return subscribedTypes;
-      }
-    };
+                return subscribedTypes;
+            }
+        };
 
-    client.addListener(listener1);
+        client.addListener(listener1);
 
-    try {
-      Thread.sleep(10000L);
-    } catch (InterruptedException e1) {
-      // TODO Auto-generated catch block
-      e1.printStackTrace();
+        try {
+            Thread.sleep(10000L);
+        } catch (InterruptedException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+
+        PolygonStreamListener listener2 = new PolygonStreamListener() {
+
+            @Override
+            public void streamUpdate(String ticker, ChannelType channelType, ChannelMessage message) {
+                System.out.println(ticker + " " + channelType + " " + message);
+            }
+
+            @Override
+            public Map<String, Set<ChannelType>> getStockChannelTypes() {
+
+                Map<String, Set<ChannelType>> subscribedTypes = new HashMap<>();
+
+                subscribedTypes.put("SNAP", Sets.newHashSet(ChannelType.values()));
+                subscribedTypes.put("AMZN", Sets.newHashSet(ChannelType.values()));
+
+                return subscribedTypes;
+            }
+        };
+
+        client.addListener(listener2);
+
+        try {
+            Thread.sleep(10000L);
+        } catch (InterruptedException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+
+        System.out.println("remove one");
+        client.removeListener(listener2);
+
+        try {
+            Thread.sleep(10000L);
+        } catch (InterruptedException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+        System.out.println("remove two");
+
+
+        client.removeListener(listener2);
+
+        try {
+            Thread.sleep(10000L);
+        } catch (InterruptedException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+        System.out.println("remove three");
+
+
+        client.removeListener(listener1);
+
+        try {
+            Thread.sleep(10000L);
+        } catch (InterruptedException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+        System.out.println("add one");
+
+        client.addListener(listener2);
+
+        try {
+            Thread.sleep(1000000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
-
-    PolygonStreamListener listener2 = new PolygonStreamListener() {
-
-      @Override
-      public void streamUpdate(String ticker, ChannelType channelType, ChannelMessage message) {
-        System.out.println(ticker + " " + channelType + " " + message);
-      }
-
-      @Override
-      public Map<String, Set<ChannelType>> getStockChannelTypes() {
-
-        Map<String, Set<ChannelType>> subscribedTypes = new HashMap<>();
-
-        subscribedTypes.put("SNAP", Set.of(ChannelType.values()));
-        subscribedTypes.put("AMZN", Set.of(ChannelType.values()));
-
-        return subscribedTypes;
-      }
-    };
-
-    client.addListener(listener2);
-
-    try {
-      Thread.sleep(10000L);
-    } catch (InterruptedException e1) {
-      // TODO Auto-generated catch block
-      e1.printStackTrace();
-    }
-
-    System.out.println("remove one");
-    client.removeListener(listener2);
-
-    try {
-      Thread.sleep(10000L);
-    } catch (InterruptedException e1) {
-      // TODO Auto-generated catch block
-      e1.printStackTrace();
-    }
-    System.out.println("remove two");
-
-
-    client.removeListener(listener2);
-
-    try {
-      Thread.sleep(10000L);
-    } catch (InterruptedException e1) {
-      // TODO Auto-generated catch block
-      e1.printStackTrace();
-    }
-    System.out.println("remove three");
-
-
-    client.removeListener(listener1);
-
-    try {
-      Thread.sleep(10000L);
-    } catch (InterruptedException e1) {
-      // TODO Auto-generated catch block
-      e1.printStackTrace();
-    }
-    System.out.println("add one");
-
-    client.addListener(listener2);
-
-    try {
-      Thread.sleep(1000000);
-    } catch (InterruptedException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
-  }
 }
