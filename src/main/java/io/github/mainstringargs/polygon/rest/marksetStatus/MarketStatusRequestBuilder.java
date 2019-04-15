9a04e24db39d297451a85004b841319a3ba8e64e
mainstringargs <mark.l.cloutier@gmail.com>
2019-04-15 08:42:54 -0500

    Adding builder for marketstatus

---
+++ src/main/java/io/github/mainstringargs/polygon/rest/marksetStatus/MarketStatusRequestBuilder.java
@@ -0,0 +1,35 @@
+package io.github.mainstringargs.polygon.rest.marksetStatus;
+
+import io.github.mainstringargs.polygon.rest.PolygonRequestBuilder;
+
+/**
+ * The Class MarketStatusRequestBuilder.
+ */
+public abstract class MarketStatusRequestBuilder extends PolygonRequestBuilder {
+
+
+  /** The Constant MARKET_STATUS_ENDPOINT. */
+  public final static String MARKET_STATUS_ENDPOINT = "marketstatus";
+
+
+
+  /**
+   * Instantiates a new market status request builder.
+   *
+   * @param baseUrl the base url
+   */
+  public MarketStatusRequestBuilder(String baseUrl) {
+    super(baseUrl);
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see io.github.mainstringargs.alpaca.rest.AlpacaUrlBuilder#endpoint()
+   */
+  @Override
+  public String getEndpoint() {
+    return MARKET_STATUS_ENDPOINT;
+  }
+
+}
