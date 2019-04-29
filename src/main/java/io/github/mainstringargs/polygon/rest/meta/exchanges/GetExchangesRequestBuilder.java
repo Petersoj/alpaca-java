fcc39d3400d6bcf53e6b396e31088ad675d0154c
mainstringargs <mainstringargs@users.noreply.github.com>
2019-04-29 16:02:16 -0500

    Adding javadocs

--- src/main/java/io/github/mainstringargs/polygon/rest/meta/exchanges/GetExchangesRequestBuilder.java
+++ src/main/java/io/github/mainstringargs/polygon/rest/meta/exchanges/GetExchangesRequestBuilder.java
@@ -3,17 +3,29 @@ package io.github.mainstringargs.polygon.rest.meta.exchanges;
 import io.github.mainstringargs.polygon.rest.PolygonRequestBuilder;
 
 
+/**
+ * The Class GetExchangesRequestBuilder.
+ */
 public class GetExchangesRequestBuilder extends PolygonRequestBuilder {
 
 
+  /** The Constant EXCHANGES_ENDPOINT. */
   public final static String EXCHANGES_ENDPOINT = "meta/exchanges";
 
 
+  /**
+   * Instantiates a new gets the exchanges request builder.
+   *
+   * @param baseUrl the base url
+   */
   public GetExchangesRequestBuilder(String baseUrl) {
     super(baseUrl);
   }
 
 
+  /* (non-Javadoc)
+   * @see io.github.mainstringargs.polygon.rest.PolygonRequestBuilder#getEndpoint()
+   */
   @Override
   public String getEndpoint() {
     return EXCHANGES_ENDPOINT;
