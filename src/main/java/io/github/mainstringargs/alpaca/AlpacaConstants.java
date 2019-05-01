565f71105812f129b1786edbef7d5f22ee905899
mainstringargs <mainstringargs@users.noreply.github.com>
2019-05-01 08:50:00 -0500

    Adding constants class for Alpaca related url variables

---
+++ src/main/java/io/github/mainstringargs/alpaca/AlpacaConstants.java
@@ -0,0 +1,85 @@
+package io.github.mainstringargs.alpaca;
+
+public class AlpacaConstants {
+
+
+  /** The Constant SYMBOLS_URL_PARAMETER. */
+  public static final String SYMBOLS_URL_PARAMETER = "symbols";
+
+  /** The Constant END_URL_PARAMETER. */
+  public static final String END_URL_PARAMETER = "end";
+
+  /** The Constant START_URL_PARAMETER. */
+  public static final String START_URL_PARAMETER = "start";
+
+  /** The Constant ASSET_CLASS_URL_PARAMETER. */
+  public static final String ASSET_CLASS_URL_PARAMETER = "asset_class";
+
+  /** The Constant CLIENT_ORDER_ID_URL_PARAMETER. */
+  public static final String CLIENT_ORDER_ID_URL_PARAMETER = "client_order_id";
+
+  /** The Constant STOP_PRICE_URL_PARAMETER. */
+  public static final String STOP_PRICE_URL_PARAMETER = "stop_price";
+
+  /** The Constant LIMIT_PRICE_URL_PARAMETER. */
+  public static final String LIMIT_PRICE_URL_PARAMETER = "limit_price";
+
+  /** The Constant TIME_IN_FORCE_URL_PARAMETER. */
+  public static final String TIME_IN_FORCE_URL_PARAMETER = "time_in_force";
+
+  /** The Constant TYPE_URL_PARAMETER. */
+  public static final String TYPE_URL_PARAMETER = "type";
+
+  /** The Constant SIDE_URL_PARAMETER. */
+  public static final String SIDE_URL_PARAMETER = "side";
+
+  /** The Constant QTY_URL_PARAMETER. */
+  public static final String QTY_URL_PARAMETER = "qty";
+
+  /** The Constant SYMBOL_URL_PARAMETER. */
+  public static final String SYMBOL_URL_PARAMETER = "symbol";
+
+  /** The Constant DIRECTION_URL_PARAMETER. */
+  public static final String DIRECTION_URL_PARAMETER = "direction";
+
+  /** The Constant UNTIL_URL_PARAMETER. */
+  public static final String UNTIL_URL_PARAMETER = "until";
+
+  /** The Constant AFTER_URL_PARAMETER. */
+  public static final String AFTER_URL_PARAMETER = "after";
+
+  /** The Constant LIMIT_URL_PARAMETER. */
+  public static final String LIMIT_URL_PARAMETER = "limit";
+
+  /** The Constant STATUS_URL_PARAMETER. */
+  public static final String STATUS_URL_PARAMETER = "status";
+
+  /** The Constant BARS_ENDPOINT. */
+  public static final String BARS_ENDPOINT = "bars";
+
+  /** The Constant CLOCK_ENDPOINT. */
+  public static final String CLOCK_ENDPOINT = "clock";
+
+  /** The Constant CALENDAR_ENDPOINT. */
+  public static final String CALENDAR_ENDPOINT = "calendar";
+
+  /** The Constant ASSETS_ENDPOINT. */
+  public static final String ASSETS_ENDPOINT = "assets";
+
+  /** The Constant POSITIONS_ENDPOINT. */
+  public static final String POSITIONS_ENDPOINT = "positions";
+
+  /** The Constant ACCOUNT_ENDPOINT. */
+  public static final String ACCOUNT_ENDPOINT = "account";
+
+  /** The Constant ORDERS_BY_CLIENT_ORDER_ID_ENDPOINT. */
+  public static final String ORDERS_BY_CLIENT_ORDER_ID_ENDPOINT = "orders:by_client_order_id";
+
+  /** The Constant ORDERS_ENDPOINT. */
+  public static final String ORDERS_ENDPOINT = "orders";
+
+  /** The Constant UTF_ENCODING. */
+  public static final String UTF_ENCODING = "UTF-8";
+
+
+}
