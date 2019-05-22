1958be286f565a5e7d6c6f67c2f0c03a08b1971d
mainstringargs <mainstringargs@users.noreply.github.com>
2019-05-22 13:17:37 -0500

    Populating Timespan enum

--- src/main/java/io/github/mainstringargs/polygon/enums/Timespan.java
+++ src/main/java/io/github/mainstringargs/polygon/enums/Timespan.java
@@ -1,5 +1,62 @@
 package io.github.mainstringargs.polygon.enums;
 
+
+/**
+ * The Enum Timespan.
+ */
 public enum Timespan {
 
+  /** The Minute. */
+  Minute("minute"),
+  /** The Hour. */
+  Hour("hour"),
+  /** The Day. */
+  Day("day"),
+  /** The Week. */
+  Week("week"),
+  /** The Month. */
+  Month("month"),
+  /** The Quarter. */
+  Quarter("quarter"),
+  /** The Year. */
+  Year("year");
+
+  /** The api name. */
+  String apiName;
+
+  /**
+   * Instantiates a new order type.
+   *
+   * @param apiName the api name
+   */
+  Timespan(String apiName) {
+    this.apiName = apiName;
+  }
+
+  /**
+   * Gets the API name.
+   *
+   * @return the API name
+   */
+  public String getAPIName() {
+    return apiName;
+  }
+
+  /**
+   * From API name.
+   *
+   * @param apiName the api name
+   * @return the channel type
+   */
+  public static Timespan fromAPIName(String apiName) {
+    String apiNameString = apiName.trim();
+
+    for (Timespan cType : Timespan.values()) {
+      if (apiNameString.equals(cType.apiName)) {
+        return cType;
+      }
+    }
+
+    return null;
+  }
 }
