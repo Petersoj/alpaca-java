c18eed8be879223f8b0757d1a77c62449d9611cf
Jacob Peterson <14127217+Petersoj@users.noreply.github.com>
2026-06-20 12:57:41 -0600

    Add root Gradle build script with JReleaser plugin

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+import org.jreleaser.model.Active.ALWAYS
+import org.jreleaser.model.Active.NEVER
+
+plugins {
+    java
+    id("org.jreleaser") version "1.24.0"
+}
+
+group = PROJECT_GROUP
+version = PROJECT_VERSION
+
+repositories {
+    mavenCentral()
+}
+
+jreleaser {
+    signing {
+        pgp {
+            active = ALWAYS
+            armored = true
+        }
+    }
+    deploy {
+        maven {
+            mavenCentral {
+                create("sonatype") {
+                    active = ALWAYS
+                    url = "https://central.sonatype.com/api/v1/publisher"
+                    stagingRepositories.add(layout.buildDirectory.dir(JRELEASER_MAVEN_REPOSITORY_DIRECTORY)
+                            .map { it.asFile.path })
+                    skipPublicationCheck = true
+                }
+            }
+        }
+    }
+    release {
+        github {
+            uploadAssets = NEVER
+        }
+    }
+}
+
+subprojects {
+    tasks.configureEach {
+        rootProject.tasks.jreleaserFullRelease.configure {
+            mustRunAfter(this@configureEach)
+        }
+    }
+}
