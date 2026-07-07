c4b24971d9646198fe6d6a8d7d11e15d008fad97
Jacob Peterson <14127217+Petersoj@users.noreply.github.com>
2026-07-07 16:17:48 -0600

    Remove old codebase that used the OkHttp client and jsonschema2pojo

--- src/main/java/net/jacobpeterson/alpaca/websocket/marketdata/MarketDataWebsocketInterface.java
+++
@@ -1,8 +0,0 @@
-package net.jacobpeterson.alpaca.websocket.marketdata;
-
-import net.jacobpeterson.alpaca.websocket.AlpacaWebsocketInterface;
-
-/**
- * {@link MarketDataWebsocketInterface} is an {@link AlpacaWebsocketInterface} for {@link MarketDataWebsocket}.
- */
-public interface MarketDataWebsocketInterface extends AlpacaWebsocketInterface {}
